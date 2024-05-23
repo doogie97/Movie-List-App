33ed4e1c5231d3cf11625865c55a64f9e975f182
Doogie <chris_97@naver.com>
2024-05-23 21:40:09 +0900

    feat: Requestable protocol 생성

---
+++ Movie-List-App/Movie-List-App/Data/Network/Requestable.swift
@@ -0,0 +1,48 @@
+//
+//  Requestable.swift
+//  Movie-List-App
+//
+//  Created by Doogie on 5/23/24.
+//
+
+import Foundation
+import Alamofire
+
+protocol Requestable {
+    var baseURL: String { get }
+    var path: String { get }
+    var headers: [String : String] { get }
+    var params: [String : Any] { get }
+    var httpMethod: HTTPMethod { get }
+    var encodingType: EncodingType { get }
+}
+
+extension Requestable {
+    func dataTask<T: Decodable>(resultType: T.Type) -> DataTask<T> {
+        return request.serializingDecodable(resultType)
+    }
+    
+    private var request: DataRequest {
+        return AF.request(self.baseURL + self.path,
+                          method: self.httpMethod,
+                          parameters: self.params,
+                          encoding: self.encodingType.parameterEncoding,
+                          headers: HTTPHeaders(self.headers)) { urlRequest in
+            urlRequest.timeoutInterval = 10
+        }
+    }
+}
+
+enum EncodingType {
+    case jsonEncoding
+    case urlEncoding
+    
+    var parameterEncoding: ParameterEncoding {
+        switch self {
+        case .jsonEncoding:
+            return JSONEncoding.default
+        case .urlEncoding:
+            return URLEncoding.default
+        }
+    }
+}
