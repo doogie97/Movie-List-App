8712b026cfdcf2771cc0a616be01021917433bfe
Doogie <chris_97@naver.com>
2024-05-24 14:08:55 +0900

    feat: CollectionView 생성 메서드 구현(layout 아직 x)

--- Movie-List-App/Movie-List-App/Presentation/Home/HomeView/HomeMovieListView.swift
+++ Movie-List-App/Movie-List-App/Presentation/Home/HomeView/HomeMovieListView.swift
@@ -38,3 +38,48 @@ extension HomeMovieListView {
             .disposed(by: disposeBag)
     }
 }
+
+//MARK: - CollectionView
+extension HomeMovieListView: UICollectionViewDataSource, UICollectionViewDelegate {
+    //Make CollectionView
+    private func createSectionCollectionView() -> UICollectionView {
+        let layout = createLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        return collectionView
+    }
+    
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { [weak self] (sectionIndex, _)  -> NSCollectionLayoutSection? in
+            guard let sectionCase = self?.viewModel?.movieSectionList[safe: sectionIndex]?.movieType else {
+                return nil
+            }
+            
+            //임시로 nil return
+            switch sectionCase {
+            case .movie:
+                return nil
+            case .series:
+                return nil
+            case .episode:
+                return nil
+            case .all:
+                return nil
+            }
+        }
+    }
+    
+    //DataSource, Delegate
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return viewModel?.movieSectionList.count ?? 0
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel?.movieSectionList[safe: section]?.movieList.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
