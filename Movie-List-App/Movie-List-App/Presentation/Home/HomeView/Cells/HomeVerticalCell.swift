d7b79113c1e3b6549d4e45cfc2ef36ee306dcefb
Doogie <chris_97@naver.com>
2024-05-25 00:24:11 +0900

    feat: horizontalCell layout 구성

--- Movie-List-App/Movie-List-App/Presentation/Home/HomeView/Cells/HomeVerticalCell.swift
+++ Movie-List-App/Movie-List-App/Presentation/Home/HomeView/Cells/HomeVerticalCell.swift
@@ -9,7 +9,57 @@ import UIKit
 import SnapKit
 
 final class HomeVerticalCell: UICollectionViewCell {
+    private lazy var posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 6
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    private lazy var titleLabel = pretendardLabel(family: .SemiBold, size: 16, lineCount: 2)
+    
+    private lazy var rightImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "chevron.right")
+        imageView.tintColor = .xButtonBG
+        
+        return imageView
+    }()
+    
     func setCellContents(movie: MovieList.Movie) {
-        self.backgroundColor = .systemBlue
+        posterImageView.setImage(movie.imageUrl)
+        titleLabel.text = movie.title
+        setLayout()
+    }
+    
+    private func setLayout() {
+        self.contentView.addSubview(posterImageView)
+        self.contentView.addSubview(titleLabel)
+        self.contentView.addSubview(rightImageView)
+        
+        posterImageView.snp.makeConstraints {
+            $0.top.leading.bottom.equalToSuperview()
+            $0.width.equalTo(50)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalTo(posterImageView.snp.trailing).inset(-16)
+            $0.trailing.equalTo(rightImageView.snp.leading).inset(-8)
+        }
+        
+        rightImageView.setContentCompressionResistancePriority(.required, for: .horizontal)
+        rightImageView.setContentHuggingPriority(.required, for: .horizontal)
+        rightImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(16)
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        posterImageView.kf.cancelDownloadTask()
+        posterImageView.image = nil
     }
 }
