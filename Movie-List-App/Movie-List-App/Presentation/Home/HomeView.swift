b20dd2f7e799e2828f21921caf8379d6e7cc0647
Doogie <chris_97@naver.com>
2024-05-24 09:54:13 +0900

    feat: 검색 textField(searchField) 구현

--- Movie-List-App/Movie-List-App/Presentation/Home/HomeView.swift
+++ Movie-List-App/Movie-List-App/Presentation/Home/HomeView.swift
@@ -16,6 +16,8 @@ final class HomeView: UIView {
         loadingView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+        
+        setLayout()
     }
     
     required init?(coder: NSCoder) {
@@ -23,4 +25,28 @@ final class HomeView: UIView {
     }
     
     private(set) lazy var loadingView = LoadingView()
+    
+    private lazy var searchField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .searchBar
+        textField.layer.cornerRadius = 20
+        textField.placeholder = "검색어를 입력해 주세요."
+        textField.leftViewMode = .always
+        textField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16, height: 0.0))
+        textField.rightViewMode = .always
+        textField.rightView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16, height: 0.0))
+
+        
+        return textField
+    }()
+    
+    private func setLayout() {
+        self.addSubview(searchField)
+        
+        searchField.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(40)
+        }
+    }
 }
