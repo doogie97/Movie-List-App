f072f75663be2bdda0a1c734efeb4e729c82257b
Doogie <chris_97@naver.com>
2024-05-25 01:25:45 +0900

    feat: keyword label 생성

--- Movie-List-App/Movie-List-App/Presentation/MovieList/MovieListView.swift
+++ Movie-List-App/Movie-List-App/Presentation/MovieList/MovieListView.swift
@@ -20,19 +20,28 @@ final class MovieListView: UIView {
     
     private(set) lazy var navigationBar = DefaultNavigationBar(title: "")
     
+    private lazy var keywordLabel = pretendardLabel(family: .SemiBold)
+    
     func setViewContents(viewModel: MovieListVMable,
                          keyword: String,
                          searchType: MovieType) {
         self.navigationBar.titleLabel.text = "\(searchType.title) 목록"
+        self.keywordLabel.text = "'\(keyword)' 검색 결과"
     }
     
     private func setLayout() {
         self.backgroundColor = .systemBackground
         self.addSubview(navigationBar)
+        self.addSubview(keywordLabel)
         
         navigationBar.snp.makeConstraints {
             $0.top.equalTo(safeAreaLayoutGuide).inset(8)
             $0.leading.trailing.equalTo(safeAreaLayoutGuide)
+        }
+        
+        keywordLabel.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom).inset(-4)
+            $0.leading.trailing.equalToSuperview().inset(24)
         }
     }
 }
