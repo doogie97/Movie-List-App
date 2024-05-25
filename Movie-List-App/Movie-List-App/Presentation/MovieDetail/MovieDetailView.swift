c3b05e9b4761cc82ced81e2c5d245f115a91a63d
Doogie <chris_97@naver.com>
2024-05-25 16:06:59 +0900

    feat: posterImageVIew, TitleLabel 생성

--- Movie-List-App/Movie-List-App/Presentation/MovieDetail/MovieDetailView.swift
+++ Movie-List-App/Movie-List-App/Presentation/MovieDetail/MovieDetailView.swift
@@ -44,12 +44,17 @@ final class MovieDetailView: UIView {
     private lazy var scrollView = UIScrollView()
     private lazy var contentsView = UIView()
     
+    private lazy var posterImageView = UIImageView()
+   
+    private lazy var titleLabel = pretendardLabel(family: .Bold, size: 20)
     private func setLayout() {
         self.backgroundColor = .systemBackground
         self.addSubview(gradientView)
         self.addSubview(navigationBar)
         gradientView.addSubview(scrollView)
         scrollView.addSubview(contentsView)
+        contentsView.addSubview(posterImageView)
+        contentsView.addSubview(titleLabel)
         
         gradientView.snp.makeConstraints {
             $0.edges.equalToSuperview()
@@ -67,6 +72,17 @@ final class MovieDetailView: UIView {
         contentsView.snp.makeConstraints {
             $0.edges.equalToSuperview()
             $0.width.equalToSuperview()
+        }
+        
+        posterImageView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(posterImageView.snp.width).multipliedBy(1.43)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(posterImageView.snp.bottom).inset(-16)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview() //다음 뷰로 이동
         }
     }
 }
