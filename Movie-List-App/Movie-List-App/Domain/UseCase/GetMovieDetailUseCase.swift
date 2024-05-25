7dc0229c0f8b193602e558bfe35aa4b49f7ae6cd
Doogie <chris_97@naver.com>
2024-05-25 16:46:24 +0900

    refactor: 영화 상세 정보 없을 경우 "알 수 없음"으로 반환

--- Movie-List-App/Movie-List-App/Domain/UseCase/GetMovieDetailUseCase.swift
+++ Movie-List-App/Movie-List-App/Domain/UseCase/GetMovieDetailUseCase.swift
@@ -16,16 +16,16 @@ struct GetMovieDetailUseCase {
         do {
             let response = try await networkRepository.getMovieDetail(movieId: movieId)
             return MovieDetail(
-                title: response.Title ?? "",
-                year: response.Year ?? "",
-                runtime: response.Runtime ?? "",
-                genre: response.Genre ?? "",
-                director: response.Director ?? "",
-                writer: response.Writer ?? "",
-                actors: response.Actors ?? "",
-                plot: response.Plot ?? "",
+                title: response.Title == "N/A" ? "알 수 없음" : (response.Title ?? ""),
+                year: response.Year == "N/A" ? "알 수 없음" : (response.Year ?? ""),
+                runtime: response.Runtime == "N/A" ? "알 수 없음" : (response.Runtime ?? ""),
+                genre: response.Genre == "N/A" ? "알 수 없음" : (response.Genre ?? ""),
+                director: response.Director == "N/A" ? "알 수 없음" : (response.Director ?? ""),
+                writer: response.Writer == "N/A" ? "알 수 없음" : (response.Writer ?? ""),
+                actors: response.Actors == "N/A" ? "알 수 없음" : (response.Actors ?? ""),
+                plot: response.Plot == "N/A" ? "알 수 없음" : (response.Plot ?? ""),
                 imageUrl: response.Poster ?? "",
-                rating: response.imdbRating ?? ""
+                rating: response.imdbRating == "N/A" ? "알 수 없음" : (response.imdbRating ?? "")
             )
         } catch let error {
             throw error
